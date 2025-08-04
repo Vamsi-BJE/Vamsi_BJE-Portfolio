f8883c1d2d8f61d6b2ddd63db407a9f36ea1ccdc
Vamsi-BJE <eppevamsi@gmail.com>
2025-08-04 19:41:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+      observer.unobserve(entry.target); 
+    }
+  });
+});
+
+document.querySelectorAll(".fade-in-on-scroll").forEach(el => {
+  observer.observe(el);
+});
